01da27871bfb0fe5891c64c52c10bc6116d2b4b5
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-03-08 10:55:01 +0100

    Update install_chrome.ps1

--- install_chrome.ps1
+++ install_chrome.ps1
@@ -1,5 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtoco
 l = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(
 'https://community.chocolatey.org/install.ps1'))
-choco -? -wait
 choco install googlechrome -y

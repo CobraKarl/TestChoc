07006c07b11562bd5a276ee3933cf0dff2a7917e
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-03-10 11:31:59 +0100

    Update install_chrome.ps1

--- install_chrome.ps1
+++ install_chrome.ps1
@@ -2,3 +2,5 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 l = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(
 'https://community.chocolatey.org/install.ps1'))
 choco install googlechrome -y
+choco install notepadplusplus.install -y
+choco install office365business --version 14729.20228 -y
